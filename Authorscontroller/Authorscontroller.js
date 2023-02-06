2f0e4fe92f591e52233b29b07088ad53c464002a
Mac kingsley <kingsleyudoma2018@gmail.com>
2023-02-06 10:44:02 +0100

    y1

--- Authorscontroller/Authorscontroller.js
+++ Authorscontroller/Authorscontroller.js
@@ -59,12 +59,13 @@ exports.follow = async (req,res)=> {
     const find = authormodel.findOne({_id:authors});
 
     if (find) {
-        const updates = authormodel.findOneAndUpdate({_id:authors}, {$push:{followers:authors}},{new:true},(error)=>{
+        const updates = await authormodel.findOneAndUpdate({_id:authors}, {$push:{followers:authors}},{new:true},(error)=>{
            // console.log(error);
             
         },res.status(200).json({
             message:"Succesfully followed User"
         }));
+       
         
     
     } else {
